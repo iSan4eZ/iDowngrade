cfd44adf045427f1c3111d08e9e2bae519af75b3
iSan4eZ <temnyip@gmail.com>
2018-07-15 18:57:36 +0300

    Initial Commit

---
+++ iDowngrade/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iDowngrade
+//
+//  Created by Stas on 7/15/18.
+//  Copyright © 2018 iSan4eZ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
